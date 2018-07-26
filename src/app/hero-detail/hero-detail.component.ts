cc62f5f0886d175a944ea46861701d2fd18add45
dodevs <douglass.sousa@outlook.com.br>
2018-07-26 17:32:33 -0300

    Added new method to display hero datails

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'; // traz informações sobre a rota para essa instancia
+import { Location } from '@angular/common'; // Serviço para interagir com o browser
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,9 +15,24 @@ export class HeroDetailComponent implements OnInit {
   // Para poder receber dados
   @Input() hero: Hero;
 
-  constructor() { }
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    // snapshot é uma imagem estática da rota após o componente ser criado
+    // paramMap é um dicionario dos parametros extraidos da url
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.getHero(id);
+  }
+
+  getHero(id): void {
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
 }
