33cc355addd188ccab032df74d7aebb905520346
dodevs <douglass.sousa@outlook.com.br>
2018-07-26 16:58:10 -0300

    Added route to dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 // Rotas
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full' // só redireciona se for exatamente vazio
+    // pathMatch: 'prefix' - redireciona se for qualquer coisa
+  },
   { // ROta para heroes
     path: 'heroes', // url
     component: HeroesComponent // componente de destino
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   }
 ];
 
