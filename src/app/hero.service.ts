be891f078c28a12c28abf0bfe9e99f6402587f07
dodevs <douglass.sousa@outlook.com.br>
2018-07-26 15:39:21 -0300

    Service-in-service to add message when heroes fetched

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs'; // RxJS Observable an of method
+
 import { Hero } from './hero'; // Class hero
 import { HEROES } from './mock-heroes'; // Mock heroes
-import { Observable, of } from 'rxjs'; //
+import { MessageService } from './message.service';
 
 // Injectable decorator com propriedades definidas
 // [providedIn:   root] o deixa disponivel para qualquer classe
 @Injectable({providedIn: 'root'})
 export class HeroService {
 
-  constructor() { }
+  // Service-in-service - Um serviço injetado em outro
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes'); // Envia a mensagem e depois entrega os heroes
     return of(HEROES); // rxjs.of() retorna um Observable [ Emite notificações a cada mudança ]
   }
 }
