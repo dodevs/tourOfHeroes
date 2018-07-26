2fb7391f146a3a545fa7d4c624e78b4d644cd979
dodevs <douglass.sousa@outlook.com.br>
2018-07-26 12:44:13 -0300

    Nixos compatibility

---
+++ default.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+    name = "nodeEnv";
+    env = buildEnv { name = name; paths = buildInputs; };
+    buildInputs = [
+        nodejs-8_x
+    ];
+    shellHook = 
+        ''
+            export PATH="$PATH:$PWD/node_modules/.bin"
+            npm install npm
+        '';
+}
