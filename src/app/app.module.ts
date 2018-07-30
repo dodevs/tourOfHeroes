9925116787dbe76e17d655771e5199061b742ff2
dodevs <douglass.sousa@outlook.com.br>
2018-07-30 10:40:22 -0300

    Simulated Web API implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -22,7 +25,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    // O HttpClientInMemoryWebApi intercepta requisições HTTP
+    // e retorna respostas do servidor simuladas
+    // Deve remover isso quando um servidor real estive rpronto para receber requisições
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false} //InMemoryDataService é o banco de dados em memória primario
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http'; //Http dependencies
+
 import { Observable, of } from 'rxjs'; // RxJS Observable an of method
 
 import { Hero } from './hero'; // Class hero
 import { HEROES } from './mock-heroes'; // Mock heroes
 import { MessageService } from './message.service';
+import { TouchSequence } from 'selenium-webdriver';
 
 // Injectable decorator com propriedades definidas
 // [providedIn:   root] o deixa disponivel para qualquer classe
 @Injectable({providedIn: 'root'})
 export class HeroService {
 
+  private heroesUrl = 'api/heroes'; // URL para a api web
+
   // Service-in-service - Um serviço injetado em outro
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes'); // Envia a mensagem e depois entrega os heroes
-    return of(HEROES); // rxjs.of() retorna um Observable [ Emite notificações a cada mudança ]
+    return this.http.get<Hero[]>(this.heroesUrl)
+    /*this.messageService.add('HeroService: fetched heroes'); // Envia a mensagem e depois entrega os heroes
+    return of(HEROES); // rxjs.of() retorna um Observable [ Emite notificações a cada mudança ]*/
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find( hero => hero.id === id)); // Retorna um observable do resultado da busca
+  }
+
+  /* Loggar uma mensagem com o serviço MessageService */
+  private log(message: string) {
+    this.messageService.add(`Hero Service: ${message}`);
   }
 }
