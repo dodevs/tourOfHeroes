190db1eb3861798104a674da7eeb67c24f8121c3
dodevs <douglass.sousa@outlook.com.br>
2018-07-30 14:47:52 -0300

    Simulated Web API implemented

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient, HttpHeaders } from '@angular/common/http'; //Http dependencies
+import { HttpClient, HttpHeaders } from '@angular/common/http'; // Http dependencies
 
 import { Observable, of } from 'rxjs'; // RxJS Observable an of method
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero'; // Class hero
 import { HEROES } from './mock-heroes'; // Mock heroes
 import { MessageService } from './message.service';
-import { TouchSequence } from 'selenium-webdriver';
 
 // Injectable decorator com propriedades definidas
 // [providedIn:   root] o deixa disponivel para qualquer classe
@@ -23,18 +23,47 @@ export class HeroService {
   ) { }
 
   getHeroes(): Observable<Hero[]> {
+    // retorna um Observable também. Um array de objeto do tipo Hero
     return this.http.get<Hero[]>(this.heroesUrl)
-    /*this.messageService.add('HeroService: fetched heroes'); // Envia a mensagem e depois entrega os heroes
-    return of(HEROES); // rxjs.of() retorna um Observable [ Emite notificações a cada mudança ]*/
+      .pipe(
+        tap( () => this.log('fetched heroes')),
+        catchError(this.handleError('getHeroes', [])) // Intercepta e trata erros se o observable falhar
+      );
+    /* return of(HEROES); // rxjs.of() retorna um Observable [ Emite notificações a cada mudança ]*/
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find( hero => hero.id === id)); // Retorna um observable do resultado da busca
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap( () => this.log(`fetched hero id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+    /* return of(HEROES.find( hero => hero.id === id)); // Retorna um observable do resultado da busca */
   }
 
   /* Loggar uma mensagem com o serviço MessageService */
   private log(message: string) {
     this.messageService.add(`Hero Service: ${message}`);
+  }
+
+  /**
+   * Trata um aoperação Http se ela falhar.
+   * Não impede o app de continuar.
+   * @param operation - nome da operação que falhou.
+   * @param result - valor opcional para ser rertornado como um observable.
+   */
+  // Tipo genereico <T>
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: enviar o erro para uma infraestrutura remote de log
+      console.error(error);
+
+      // TODO: um trabalho melhor na transformação do erro para consumo do usuário
+      this.log(`${operation} failed: ${error.message}`);
+
+      // O aplicativo continua funcionando retornando um resultado vazio.
+      return of(result as T);
+    };
   }
 }
